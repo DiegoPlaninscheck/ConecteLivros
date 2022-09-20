493eab7061876185489aa6e0def057b79949c680
Diego <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-09-20 20:55:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ConecteLivros
+ 
---
+++ src/br/senai/sc/livros/model/service/PessoaService.java
@@ -0,0 +1,34 @@
+package br.senai.sc.livros.model.service;
+
+import br.senai.sc.livros.model.dao.PessoaDAO;
+import br.senai.sc.livros.model.entities.Pessoa;
+import br.senai.sc.livros.model.factory.ConexaoFactory;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class PessoaService {
+    PessoaDAO bdPessoa = new PessoaDAO();
+
+    public void inserir(Pessoa pessoa) {
+        try{
+            bdPessoa.inserir(pessoa);
+        } catch (SQLException err){
+            JOptionPane.showMessageDialog(null, "Erro: " + err.getMessage());
+            System.out.println(err.getMessage());
+        }
+    }
+
+    public void remover(Pessoa pessoa) {
+        bdPessoa.remover(pessoa);
+    }
+
+    public Pessoa selecionarPorCPF(String CPF) {
+        return bdPessoa.selecionarPorCPF(CPF);
+    }
+
+    public Pessoa selecionarPorEmail(String email){
+        return bdPessoa.selecionarPorEmail(email);
+    }
+}
